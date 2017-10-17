7672d09dfbcbb2990cae6e90f51c9659ef48386b
Fabio Tudone <fabio_tudone@bmc.com>
2017-10-17 17:44:38 +0300

    :arrow_down: Downgrade to Spring Boot 1.3.x with latest MongoDB V2 Java driver

--- src/main/java/works/weave/socks/orders/config/PrometheusAutoConfiguration.java
+++ src/main/java/works/weave/socks/orders/config/PrometheusAutoConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.endpoint.PublicMetrics;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
